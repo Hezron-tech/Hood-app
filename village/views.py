c0b9bbc97424bc2a33bd5a56a770d8df6d1a0f9d
Hezron-tech <kiprophez@gmail.com>
2022-04-17 11:30:52 +0300

    creating search fucntion by location

--- village/views.py
+++ village/views.py
@@ -26,19 +26,19 @@ def dashboard(request):
     return render(request,'dashboard.html',context)
 
 
-# # search neighborhood by location   
-# def search_hood(request):
-#     if 'search' in request.GET and request.GET["search"]:
-#         search_term = request.GET.get("search")
-#         print(search_term)
-#         hoods = Neighborhood.search_hood(search_term)
-#         message = f"{search_term}"
+# search neighborhood by location   
+def search_hood(request):
+    if 'search' in request.GET and request.GET["search"]:
+        search_term = request.GET.get("search")
+        print(search_term)
+        hoods = Neighborhood.search_hood(search_term)
+        message = f"{search_term}"
 
-#         return render(request, 'search.html',{'hoods':hoods})
+        return render(request, 'search.html',{'hoods':hoods})
 
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'search.html',{"message":message})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
 
 # # Home posts and details
 # @login_required(login_url='/accounts/login/')
